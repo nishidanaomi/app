1cb38ba5f4e755af29dc2e54b772e14916dd534e
N <dnaominishida@gmail.com>
2024-09-11 11:23:27 -0300

    menu aplicação

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const start = () => {
-    let count = 1
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 2L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.leight == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+
+    metas.push(
+         { value: meta, checked: false }
+        )
+}
+
+const start = async () => {
+    
     while(true){
-        let opcao = "cadastrar"
+
+        const opcao = await select({
+        })
+
+
         switch(opcao) {
-            case "cadastrar":
-                console.log("vamos cadastrar")
+            case "Cadastrar":
+                await cadastrarMeta()        
+                console.log(metas)
                 break
-            case "listar":
+            case "Listar":
                 console.log("vamos listar")
                 break
                 case "sair":
+                    console.log("Até a próxima!")
                     return
     
         
